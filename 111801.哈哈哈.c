16991dc9d00885838454536673b620c3336ff991
fcjackson <74294700+fcjackson@users.noreply.github.com>
2020-11-18 09:19:36 +0800

    Create 111801.哈哈哈.c

---
+++ 111801.哈哈哈.c
@@ -0,0 +1,81 @@
+5).
+#include<stdio.h>
+void main(){
+	int a=3;
+	int b=4;
+	int c=5;
+	if(!(a+b)+c-1 && b+c/2){
+	printf("为真命题");
+	}
+    else{
+    printf("为假命题");
+	}
+}
+
+
+
+(4).
+#include<stdio.h>
+void main(){
+	int a=3;
+	int b=4;
+	int c=5;
+  int x;
+  int y;
+	if(!(x=a) && (y=b) && 0){
+	printf("为真命题");
+	}
+    else{
+    printf("为假命题");	
+	}
+}
+
+
+
+(3).
+#include<stdio.h>
+void main(){
+	int a=3;
+	int b=4;
+	int c=5;
+	if(!(a>b) && !c||1){
+	printf("为真命题");
+	}
+    else{
+    printf("为假命题");
+	  }
+  }
+  
+  
+  
+  (2).
+  #include<stdio.h>
+void main(){
+	int a=3;
+	int b=4;
+	int c=5;
+	if(a||b+c && b-c){
+	printf("为真命题");
+	}
+    else{
+    printf("为假命题");
+	
+	}
+}
+  
+  
+  (1).
+  #include<stdio.h>
+void main(){
+	int a=3;
+	int b=4;
+	int c=5;
+	if(a+b>c && b==c){
+	printf("为真命题");
+	}
+    else{
+    printf("为假命题");
+	
+	}
+}
+  
