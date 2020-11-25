ac90823becf48e7db15b0005e8fc4a62571bfd7d
fcjackson <74294700+fcjackson@users.noreply.github.com>
2020-11-25 09:30:26 +0800

    Create 112501.打工人工资.app.c

---
+++ 112501.打工人工资.app.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main(){
+int a;
+scanf("%d",&a);
+if(a>=0 && a<=100){
+printf("贫困打工人\n");
+}
+else if(a>=101 && a<=1000){
+printf("普通打工人\n");
+}
+else if(a>=1001 && a<=20000){
+printf("土豪打工人\n");
+}
+else{
+printf("输入错误，请重新输入。\n");
+}
+
+
+
+}
