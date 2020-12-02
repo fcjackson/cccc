51519190419fc49836b0a095c950ab5bac74c5c3
fcjackson <74294700+fcjackson@users.noreply.github.com>
2020-12-02 09:36:30 +0800

    Update 120202.字符判断.app.c

--- 120202.字符判断.app.c
+++ 120202.字符判断.app.c
@@ -4,10 +4,10 @@ void main(){
   printf("欢迎使用此程序\n");
   scanf("%c",&a);
   switch(a){
-  case 'a':printf("你在点击'上'健\n");break;
+  case 'w':printf("你在点击'上'健\n");break;
   case 's':printf("你在点击'下'健\n");break;
-  case 'd':printf("你在点击'左'健\n");break;
-  case 'w':printf("你在点击'右'健\n");break;
+  case 'a':printf("你在点击'左'健\n");break;
+  case 'd':printf("你在点击'右'健\n");break;
   default:printf("你点啥呢\n");
 }
 }
