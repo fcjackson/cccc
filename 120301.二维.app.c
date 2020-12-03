0541346d6e296217c34be0f965f927e355c97968
fcjackson <74294700+fcjackson@users.noreply.github.com>
2020-12-03 20:46:11 +0800

    Create 120301.二维.app.c

---
+++ 120301.二维.app.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+void main(){
+ int i,n;
+ int a[]={2001,2002,2003,2004};
+ int a2d[3][3]={
+    {1,2,3},
+    {4,5,6},
+    {7,8,9}
+};
+ for(i=0;i<3;i++){
+ for(n=0;n<3;n++){
+    printf("%d ",a2d[i][n]);
+ }
+ printf("\n");
+  }
+
+
+ }
+
